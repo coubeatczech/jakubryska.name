2a0fa85ee83f84cd7b4bd709957baa998a835896
Jakub Ryška <ryskajakub@seznam.cz>
2014-06-04 14:51:06 +0200

    remove about and dead links in default template

--- site.hs
+++ site.hs
@@ -13,7 +13,7 @@ main = hakyll $ do
     route idRoute
     compile compressCssCompiler
 
-  match (fromList ["about.rst", "contact.markdown"]) $ do
+  match "contact.markdown" $ do
     route $ setExtension "html"
     compile $ pandocCompiler
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
